e7b41de9ba9f958623a30e5f3d14d4c268f4de66
Ijnacio <ig.sobarzo@duocuc.cl>
2025-10-22 17:00:45 -0300

    - Se crea la estructura de carpetas ui/screens y ui/navigation. - Se añaden las 3 pantallas V1 (Feed, Agregar, Detalle). - Se añade la dependencia de Navigation Compose. - Se configura AppNavigation.kt con el NavHost y las 3 rutas. - Se limpia MainActivity.kt y se llama a AppNavigation. - Se configura la paleta de colores pastel en Theme.kt y Color.kt.

---
+++ app/src/main/java/cl/aplicacion/adoptapet/ui/screens/FeedScreen.kt
@@ -0,0 +1,9 @@
+package cl.aplicacion.adoptapet.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FeedScreen() {
+    Text(text = "Pantalla: Feed de Mascotas")
+}
--- app/src/main/java/cl/aplicacion/adoptapet/ui/theme/Color.kt
+++ app/src/main/java/cl/aplicacion/adoptapet/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package cl.aplicacion.adoptapet.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// --- Definimos nuestros colores pasteles ---
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val NaranjoPastel = Color(0xFFFDECDD)
+val NaranjoFuerte = Color(0xFFF9A86E)
+
+val VerdeAgua = Color(0xFFD4EAE7)
+val VerdeFuerte = Color(0xFF67BBAF)
+
+val GrisClaro = Color(0xFFF5F5F5)
+val TextoOscuro = Color(0xFF333333)
